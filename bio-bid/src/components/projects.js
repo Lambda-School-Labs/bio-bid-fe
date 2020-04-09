31adfe060e4d54496b31ffd75c6cc5dff71ec362
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-04-09 12:08:33 -0500

    formatting fixed

--- bio-bid/src/components/projects.js
+++ bio-bid/src/components/projects.js
@@ -1,38 +1,38 @@
 import React from "react";
 import styled from "styled-components";
 
-import {useQuery} from '@apollo/react-hooks';
-import {GET_STUDIES} from '../queries';
+import { useQuery } from '@apollo/react-hooks';
+import { GET_STUDIES } from '../queries';
 import ProjectCard from './ProjectCard';
 
-function Projects(){
-    const {loading, data, error} = useQuery(GET_STUDIES);
-   return (
-       <ProjectWrapper>
-           <ProjectsTittle> Current Projects </ProjectsTittle>
-           <Cards>
-                {loading ? <p>Loading...</p>: 
-                data.studies.map(study =>
-                    <ProjectCard
-                        key={study.id}
-                        name={study.name}
-                        area={study.area}
-                        phase={study.phase}/>)}
-           </Cards>
+function Projects() {
+    const { loading, data, error } = useQuery(GET_STUDIES);
+    return (
+        <ProjectWrapper>
+            <ProjectsTittle> Current Projects </ProjectsTittle>
+            <Cards>
+                {loading ? <p>Loading...</p> :
+                    data.studies.map(study =>
+                        <ProjectCard
+                            key={study.id}
+                            name={study.name}
+                            area={study.area}
+                            phase={study.phase} />)}
+            </Cards>
 
-       </ProjectWrapper>
+        </ProjectWrapper>
 
     )
 };
 
-const ProjectWrapper= styled.div
-`
+const ProjectWrapper = styled.div
+    `
 width: 30%;
 heigth: 25%;
 `
 
 const ProjectsTittle = styled.h1
-`
+    `
 font-family: Lato;
 font-style: normal;
 font-weight: bold;
@@ -42,10 +42,10 @@ margin:10%;
 `
 
 const Cards = styled.div
-`
+    `
 display:flex;
 flex-direction: column;
 justify-content: space-between;
 height: 20%;
 `
- export default Projects;
+export default Projects;
