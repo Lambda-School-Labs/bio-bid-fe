fc2b66a129dc82d5d1abd047e811ecffd79dcae7
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-04-14 16:55:53 -0500

    took out apollo client stuff

--- bio-bid/src/index.js
+++ bio-bid/src/index.js
@@ -4,16 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import {ApolloProvider} from 'react-apollo';
-import ApolloClient from 'apollo-boost';
-
-const client = new ApolloClient({uri: 'http://ec2-34-195-186-223.compute-1.amazonaws.com/'})
-
-ReactDOM.render(
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>,
-  document.getElementById('root')
+ReactDOM.render(<App />, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
