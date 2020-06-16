11b54bf90dbc0efa6313f04fc677ae8608e4951e
Gavin S <stahlgavin@gmail.com>
2020-06-16 14:29:04 -0700

    removing claim from company list

--- bio-bid/src/components/company-profile/List/CompanyCard.js
+++ bio-bid/src/components/company-profile/List/CompanyCard.js
@@ -2,51 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import defaultLogo from "../../../images/default-company-logo.png";
 import { CompanyCard, CardButton } from "./styles";
-import { useOktaAuth } from "@okta/okta-react";
-import { CLAIM_COMPANY } from "../../../mutations";
-import { useMutation } from "@apollo/react-hooks";
 
 export default ({ company }) => {
-  const { authState, authService } = useOktaAuth();
-  const [userInfo, setUserInfo] = useState({});
-  const [addClaim] = useMutation(CLAIM_COMPANY);
-  const [isClaiming, setIsClaiming] = useState(false);
-  // console.log("already claiming: ", isClaiming);
-
-  useEffect(() => {
-    setIsClaiming(localStorage.getItem("isClaiming"));
-  }, []);
-
-  useEffect(() => {
-    authService.getUser().then(setUserInfo);
-  }, [authService]);
-  // console.log("users info: ", userInfo);
-
-  const handleClaims = async () => {
-    try {
-      localStorage.setItem("isClaiming", true);
-      setIsClaiming(true);
-      await addClaim({
-        variables: {
-          user: userInfo.sub,
-          email: userInfo.email,
-          name: `${userInfo.given_name} ${userInfo.family_name}`,
-          company: company.id,
-        },
-      });
-      // console.log(
-      //   `${userInfo.given_name} ${userInfo.family_name} created a claim for company ${company.id}`
-      // );
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  // const handleCancel = () => {
-  //   localStorage.removeItem('isClaiming');
-  //   setIsClaiming(false);
-  // }
-
   const [overview, setOverview] = useState("");
   const [tooLong, setTooLong] = useState(false);
   console.log(company.overview);
@@ -85,18 +42,6 @@ export default ({ company }) => {
             )}
           </div>
           <div className="btn-container">
-            {!authState.isAuthenticated ||
-            isClaiming ||
-            userInfo.profile === "Admin" ? null : (
-              <CardButton onClick={handleClaims} gray>
-                <p>Claim</p>
-              </CardButton>
-            )}
-            {/* {!isClaiming ? null : (
-              <CardButton onClick={handleCancel} gray>
-                <p>Cancel</p>
-              </CardButton>
-            )} */}
             <Link to={`/service-providers/${company.id}`}>
               <CardButton>
                 <p>Details</p>
