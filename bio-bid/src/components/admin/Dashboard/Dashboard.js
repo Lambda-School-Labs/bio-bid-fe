88a26ce267e499e05cdd27434b37c9442b501048
Gavin S <stahlgavin@gmail.com>
2020-06-22 14:12:21 -0700

    forgot a console log

--- bio-bid/src/components/admin/Dashboard/Dashboard.js
+++ bio-bid/src/components/admin/Dashboard/Dashboard.js
@@ -20,7 +20,7 @@ export default () => {
   useEffect(() => {
     authService.getUser().then(setUserInfo);
   }, [authService]);
-  console.log(userInfo);
+//   console.log(userInfo);
 
   return (
     <>
